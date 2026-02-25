ba1c934733b6eb559f1fe89c9f3b79a83c7e2563
Hifzon Mursalin <6242572+hifzonx8@users.noreply.github.com>
2026-02-25 22:42:06 +0700

    feat: change page title
    and some spacing

--- index.php
+++ index.php
@@ -4,13 +4,15 @@ $prod = select('select * from lItem');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="style/main.css" type="text/css" media="all" />
-  <title>Cashiee</title>
+  <title>Cashier App</title>
 </head>
+
 <body>
   <div class="container">
     <form action="funcs/main.php" method="post" accept-charset="utf-8">
@@ -25,4 +27,5 @@ $prod = select('select * from lItem');
     </form>
   </div>
 </body>
+
 </html>
